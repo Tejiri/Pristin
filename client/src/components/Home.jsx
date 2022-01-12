8a49c4c51c692a6b2528027c40b7ed25d0a4b684
Tejiri Emoghene-Ijatomi <steveijatomi@gmail.com>
2022-01-12 16:12:17 +0100

    first

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -105,8 +105,8 @@ function Home() {
                     style={{
                       backgroundColor: "#0a2332",
                       color: "white",
-                      marginBottom: "20px",
-                      padding: "10px 20px",
+                      marginBottom: "20px 20px",
+                      padding: "10px",
                       width: "200px",
                     }}
                   >
